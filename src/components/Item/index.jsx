1898b99080a57ba7c496aa1c626108edcc8b9922
Fanny <lifangning1234@yahoo.com>
2021-04-28 12:12:06 -0400

    todo-app: mouseEnter to highlight, mouseLeave to de-highlight

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,16 +2,26 @@ import React, {Component} from 'react'
 
 
 export default class Item extends Component {
+
+    state = {mouse: false}
+
+    handleMouse = (flag) => {
+        return () => {
+            this.setState({mouse: flag})
+        }
+    }
+
     render() {
         const {name, done} = this.props
+        const {mouse} = this.state
         return(
-            <li>
+            <li style={{backgroundColor: mouse ? '#61DAFB': ''}} onMouseLeave={this.handleMouse(false)} onMouseEnter={this.handleMouse(true)}>
                 <div style={{float:'left'}}>
                     <input type="checkbox" defaultChecked={done}/>
                     <span>{name}</span>
                 </div>
                 <div style={{float:'right'}}>
-                    <button style={{backgroundColor: '#f44336', border: 'none'}}>delete</button>
+                    <button style={{ display: mouse? 'block': 'none', backgroundColor: '#f44336', border: 'none', height: 32}}>delete</button>
                 </div>
                 <br />
             </li>
